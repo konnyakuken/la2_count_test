0d15933b5c58f7817eca2e1b4a9e5f2b8191e281
konnyakuken <t0516wakamiya0402@icloud.com>
2021-08-30 06:19:10 +0000

    Implement feature: creating new counter

--- app.rb
+++ app.rb
@@ -8,3 +8,50 @@ before do
     Count.create(number: 0)
   end
 end
+
+get "/" do
+  @number=Count.find(1).number
+  erb :index
+end
+
+post "/plus" do
+  num=Count.find(1)
+  num.number +=1
+  num.save
+  redirect "/"
+end
+
+post "/minus" do
+  num=Count.find(1)
+  num.number -=1
+  num.save
+  redirect "/"
+end
+
+post "/clear" do
+  num=Count.find(1)
+  num.number =0
+  num.save
+  redirect "/"
+end
+
+post "/kakeru" do
+  num=Count.find(1)
+  num.number *=2
+  num.save
+  redirect "/"
+end
+
+post "/waru" do
+  num=Count.find(1)
+  num.number /=2
+  num.save
+  redirect "/"
+end
+
+post "/plus2" do
+  num=Count.find(2)
+  num.number +=1
+  num.save
+  redirect "/"
+end
